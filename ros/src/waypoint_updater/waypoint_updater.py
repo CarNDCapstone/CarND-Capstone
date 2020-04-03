48ad4b1845a3b2d4db8e49077b9e30741c3e9410
Marek Kolodziej <mkolod@gmail.com>
2020-04-03 13:08:33 -0700

    Waypoint updater at 10 Hz for Udacity workspace

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -50,8 +50,7 @@ class WaypointUpdater(object):
 
 
     def loop(self):
-        # Rate: 50 Hz
-        rate = rospy.Rate(15)
+        rate = rospy.Rate(10)
         while not rospy.is_shutdown():
             if self.pose and self.base_lane:
                 self.publish_waypoints()
